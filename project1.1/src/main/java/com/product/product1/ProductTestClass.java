f5a77cda53f0a064d651899a742c4a44d61e26a2
Mahima Choudhary <mahimachoudhary971@gmail.com>
2022-07-25 22:29:16 +0530

    "first commit"

---
+++ project1.1/src/main/java/com/product/product1/ProductTestClass.java
@@ -0,0 +1,13 @@
+package com.product.product1;
+import java.io.*;
+import org.junit.jupiter.api.Test;
+public class ProductTestClass {
+@Test
+void test1() {
+	System.out.println("not yet implemented");
+}
+void test2() {
+	System.out.println("not yet implemented");
+}
+
+}
